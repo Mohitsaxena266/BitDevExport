e9ad4835373a3f62e7f8c27ab5549ff784b8a920
Mohit Saxena <mohit@hbssweb.com>
2021-05-20 12:17:32 +0530

    BitDevExport

---
+++ dist/src/components/product-list/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactNative = require("react-native");
+
+var _jsxRuntime = require("react/jsx-runtime");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var MyComponent = function MyComponent() {
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_reactNative.View, {
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_reactNative.Text, {
+      children: "Mybit Data"
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_reactNative.Button, {
+      title: "Button Value"
+    })]
+  });
+};
+
+var _default = MyComponent;
+exports["default"] = _default;
+
+//# sourceMappingURL=index.js.map
---
+++ src/components/product-list/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+const MyComponent = () => {
+  return (
+    <View>
+      <Text>Mybit Data</Text>
+      <Button title="Button Value" />
+    </View>
+  );
+};
+export default MyComponent;
